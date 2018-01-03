0a26fae356454cc161bc3e4be0f7a3373bdb6751
rohit_mathew <rohit_mathew@apple.com>
2018-01-02 18:53:08 -0800

    Add LCS and LIS DP

--- DP/lcs.rb
+++ DP/lcs.rb
@@ -1,22 +1,30 @@
+# Longest Common Subsequence - Ruby Implementation
+# This Algorithm requires O(mn) space and O(mn) time 
 require 'matrix'
 
 def max arg1, arg2
   arg1 > arg2 ? arg1 : arg2
 end
-# Brute Force method
-def lcs_brute str1, str2, ptr1, ptr2, matrix
+# Dp using a hash
+def lcs str1, str2, ptr1, ptr2, hash
   if (ptr1 < 0 || ptr2 < 0) 
     return 0 
   end
-  if str1[ptr1] == str2[ptr2]
-    return 1 + lcs(str1, str2, ptr1 - 1, ptr2 - 1, matrix)
+  if hash[[ptr1, ptr2]] == -1
+    result = -1
+    if str1[ptr1] == str2[ptr2]
+      result = 1 + lcs(str1, str2, ptr1 - 1, ptr2 - 1, hash) 
+    else
+      result =  max(lcs(str1, str2, ptr1 - 1, ptr2, hash), lcs(str1, str2, ptr1, ptr2 - 1, hash))
+    end
+    hash[[ptr1, ptr2]] = result
   else
-    return max(lcs(str1, str2, ptr1 - 1, ptr2, matrix), lcs(str1, str2, ptr1, ptr2 - 1, matrix))
+    result = hash[[ptr1, ptr2]]
   end
+    return result
 end
 
 str1 = "ABCDGH"
 str2 = "AEDFHR"
-# Initialize a matrix of all zeros
-matrix = Matrix.build(str1.length, str2.length) {|m, n| 0}
-puts lcs(str1, str2, str1.length - 1, str2.length - 1, matrix)
+hash = Hash.new(-1)
+puts lcs(str1, str2, str1.length - 1, str2.length - 1, hash)
